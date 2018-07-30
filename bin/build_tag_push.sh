9699ff63c62b2c116aab712ce8b7f0c30e228be4
James O'Beirne <james.obeirne@gmail.com>
2018-07-30 16:50:38 -0400

    Using cache during build should be fine

--- bin/build_tag_push.sh
+++ bin/build_tag_push.sh
@@ -4,6 +4,6 @@ set -exuo pipefail
 
 TAG=${1:-latest}
 
-docker build --no-cache -t jamesob/bitcoind:${TAG} .
+docker build -t jamesob/bitcoind:${TAG} .
 docker login
 docker push jamesob/bitcoind:${TAG}
