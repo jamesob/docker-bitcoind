c43dba3768c9d34358f173a4f68e1788bfde945f
rmathijn <rm.andela@gmail.com>
2022-06-27 20:52:27 +0200

    Add additional versions

--- bin/get-bitcoin.sh
+++ bin/get-bitcoin.sh
@@ -27,6 +27,12 @@ VERSIONS=(
 0.19.1
 0.20.0
 0.20.1
+0.20.2
+0.21.0
+0.21.1
+0.21.2
+22.0
+23.0
 )
 
 err() {
@@ -60,21 +66,29 @@ fi
 TMPDIR=$(mktemp -d)
 cd "$TMPDIR"
 
-# Verify this signing key fingerprint here:
+# Verify signing key fingerprints here:
 #
-#   https://github.com/bitcoin/bitcoin/tree/master/contrib/verifybinaries
-#
-gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 01EA5486DE18A882D4C2684590C8019E36C2E964
+#   https://github.com/bitcoin/bitcoin/tree/master/contrib/builder-keys
 
 curl -O "${URL_BASE}/SHA256SUMS.asc"
 curl -O "${URL_BASE}/${FILENAME}"
 
-sha256sum --ignore-missing --check SHA256SUMS.asc \
-  | tee - | grep -o "${FILENAME}: OK"
-
-gpg --verify SHA256SUMS.asc >gpg_verify_out 2>&1
-grep '^gpg: Good signature from "Wladimir J. van der Laan' gpg_verify_out
-grep '^Primary key fingerprint: 01EA 5486 DE18 A882 D4C2  6845 90C8 019E 36C2 E964' gpg_verify_out
+if [[ "$VERSION" == "0."* ]]; then
+  gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 01EA5486DE18A882D4C2684590C8019E36C2E964
+  sha256sum --ignore-missing --check SHA256SUMS.asc \
+    | tee - | grep -o "${FILENAME}: OK"
+  gpg --verify SHA256SUMS.asc >gpg_verify_out 2>&1
+  grep '^gpg: Good signature from "Wladimir J. van der Laan' gpg_verify_out
+  grep '^Primary key fingerprint: 01EA 5486 DE18 A882 D4C2  6845 90C8 019E 36C2 E964' gpg_verify_out
+else
+  gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 71A3B16735405025D447E8F274810B012346C9A6
+  curl -O "${URL_BASE}/SHA256SUMS"
+  sha256sum --ignore-missing --check SHA256SUMS \
+    | tee - | grep -o "${FILENAME}: OK"
+  gpg --verify SHA256SUMS.asc SHA256SUMS >gpg_verify_out 2>&1 || true
+  grep '^gpg: Good signature from "Wladimir J. van der Laan' gpg_verify_out
+  grep '^Primary key fingerprint: 71A3 B167 3540 5025 D447  E8F2 7481 0B01 2346 C9A6' gpg_verify_out
+fi
 
 tar -xzvf "${FILENAME}"
 DIR=$(find . -name 'bitcoin-*' -type d | head -n 1)
